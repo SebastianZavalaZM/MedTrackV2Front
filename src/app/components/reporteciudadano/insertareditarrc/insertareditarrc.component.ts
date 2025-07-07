845dec0a4ace50123a6983a087912793ca8c743a
Denisse1728 <denisseevangelista1@gmail.com>
2025-07-06 19:31:02 -0500

    se arreglo el registrar de reporte y articulo

--- src/app/components/reporteciudadano/insertareditarrc/insertareditarrc.component.ts
+++ src/app/components/reporteciudadano/insertareditarrc/insertareditarrc.component.ts
@@ -81,7 +81,7 @@ export class InsertareditarrcComponent implements OnInit {
          })
        })
      }
-      this.router.navigate(['reporteciudadano'])
+      this.router.navigate(['reporteciudadano/listas'])
     }
   }
   init(){
