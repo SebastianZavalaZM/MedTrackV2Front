62d8ae18f103aa1ecf98b1e8f46ae976c5aa084d
Saul <u20221a320@upc.edu.pe>
2025-07-07 13:01:30 -0500

    home mejorado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  stats = [
+    { number: '1,000+', label: 'Usuarios Activos' },
+    { number: '500+', label: 'Reportes Procesados' },
+    { number: '50+', label: 'Enfermedades Monitoreadas' },
+    { number: '24/7', label: 'Monitoreo Continuo' }
+  ];
 }
